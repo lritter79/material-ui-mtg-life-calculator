bb4224bbc0acd1ab3769fdddb8c766684403f977
Levon Ritter <levong2112@gmail.com>
2021-08-17 14:25:39 -0400

    added colorful borders

--- src/Components/LifeCounter.js
+++ src/Components/LifeCounter.js
@@ -1,17 +1,52 @@
 import { CategoryContext } from '../App';
 import { useContext, useState, useEffect } from 'react';
+import { Typography, Box } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
-function LifeCounter({player}){
+
+
+
+const useStyles = makeStyles({
+  lifeCounterBox: {    
+    borderRadius: '10px',
+    marginBottom: '3vh'
+  },
+  blue: {
+    border: `1px solid blue`,
+  },
+  green: {
+    border: `1px solid green`,
+  },
+  red: {
+    border: `1px solid red`,
+  },
+  yellow: {
+    border: `1px solid yellow`,
+  },
+  white: {
+    border: `1px solid white`,
+  },
+})
+
+function LifeCounter({player, color}){
+  console.log(color)
+
   const selectedCategory = useContext(CategoryContext)
   const [startingLife, setStartingLife] = useState(selectedCategory.startingLife)
+
+  const classes = useStyles(color)
+  console.log(classes.lifeCounterBox)
   useEffect(() => {
     setStartingLife(selectedCategory.startingLife)
   }, [selectedCategory])
     return (
-        <div>
-          {player}
+        <Box className={`${classes.lifeCounterBox} ${classes[color]}`}>
+          <Typography variant='h6'>
+          Player {player + 1}
+          </Typography>
+          
           {startingLife}
-        </div>
+        </Box>
       );
 }
 
--- src/Components/LifeCounterContainer.js
+++ src/Components/LifeCounterContainer.js
@@ -1,15 +1,33 @@
 import LifeCounter from './LifeCounter';
 
+
+const getColorFromPlayerNumber = (player) => {
+  console.log(player)
+  switch(player) {
+      case 0:
+        return 'blue'
+      case 1:
+        return 'red'
+      case 2:
+        return 'green'
+      case 3: 
+        return 'yellow'
+      default:
+        return 'white'
+  } 
+}
+
 function LifeCounterContainer({numberOfPlayers, setNumberOfPlayers}){
+  //const [players, setPlayers] = useState([])
 
   //see: https://stackoverflow.com/questions/22876978/loop-inside-react-jsx
   var players = [];
   for (var i = 0; i < numberOfPlayers; i++) {
       // note: we are adding a key prop here to allow react to uniquely identify each
       // element in this array. see: https://reactjs.org/docs/lists-and-keys.html
-      players.push(<LifeCounter key={i} player={i} />);
+      players.push(<LifeCounter key={i} player={i} color={getColorFromPlayerNumber(i)} />);
   }
-  console.log(players)
+  //console.log(players)
       return (
         <div>
           {players}
