0ea5558768d0c4c3f447919f02cf031e600551d0
Levon Ritter <levong2112@gmail.com>
2021-10-05 00:22:59 -0400

    added spinning die

--- src/Components/LifeCounter/LifeDie.js
+++ src/Components/LifeCounter/LifeDie.js
@@ -1,6 +1,8 @@
 import { makeStyles } from "@material-ui/core/styles";
+import { useEffect } from "react";
 import DieSvg from "./DieSvg";
 import LifeTotalTypography from "./LifeTotalTypography";
+import { useSpring, animated } from "react-spring";
 
 const useStyles = makeStyles((theme) => {
   return {
@@ -26,6 +28,7 @@ const useStyles = makeStyles((theme) => {
       position: "relative",
     },
     position: {
+      zIndex: 1,
       position: "absolute",
       marginLeft: "auto",
       marginRight: "auto",
@@ -38,13 +41,25 @@ const useStyles = makeStyles((theme) => {
 });
 function LifeDie({ currentLife, color }) {
   const classes = useStyles({ color });
+  // from {transform:rotate(0deg);}
+  // to {transform:rotate(360deg);}
+  // useEffect(() => {
+  //   if (currentLife <= 0) {
+  //     console.log("dead");
+  //   }
+  // }, [currentLife]);
   const polygonStyle = { fill: color, strokeWidth: 1 };
+  const contentProps = useSpring({
+    transform: currentLife >= 1 ? "rotate(0deg)" : "rotate(360deg)",
+  });
   return (
     <div className={classes.container}>
       <div className={classes.position}>
         <LifeTotalTypography color={color} currentLife={currentLife} />
       </div>
-      <DieSvg color={color} />
+      <animated.div style={contentProps}>
+        <DieSvg color={color} />
+      </animated.div>
     </div>
   );
 }
--- src/Components/LifeCounter/LifeTotalTypography.js
+++ src/Components/LifeCounter/LifeTotalTypography.js
@@ -19,7 +19,7 @@ const useStyles = makeStyles((theme) => {
             return "white";
           }
         } else {
-          return "red";
+          return "white";
         }
       },
     },
